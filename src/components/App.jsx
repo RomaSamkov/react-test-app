d09affb7987bfafc1da2995262b1effe96e2996a
RomaSamkov <Romansamkov.rs@gmail.com>
2022-08-26 14:05:03 +0300

    add updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,19 @@
-import { AppBar } from './AppBar/AppBar';
-import { useState } from 'react';
+const arr = ['apple', 'mango', 'apple', 'orange', 'mango', 'mango'];
 
-const user = {
-  userName: 'Bob',
-  isOnline: true,
-  points: {
-    total: 700,
-    required: 200,
-  },
-};
+function removeDuplicates(arr) {
+  let unique = arr.reduce(function (acc, curr) {
+    if (!acc.includes(curr)) acc.push(curr);
+    return acc;
+  }, []);
+  return unique;
+}
+
+console.log(removeDuplicates(arr));
+
 export const App = () => {
-  const [isAppbarOpen, setIsAppbarOpen] = useState(false);
-
-  const openAppBar = () => setIsAppbarOpen(true);
-  const closeAppBar = () => setIsAppbarOpen(false);
-
   return (
     <div>
-      {!isAppbarOpen && (
-        <button type="button" onClick={openAppBar}>
-          Open
-        </button>
-      )}
-      {isAppbarOpen && <AppBar user={user} onClose={closeAppBar} />}
+      <button type="button">Open</button>
     </div>
   );
 };
--- src/components/AppBar/AppBar.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const SidebarContainer = styled.div`
-  height: 100vh;
-  width: 320px;
-  background-color: #fff;
-  box-shadow: rgb(0 0 0 / 20%) 0px 2px 4px -1px,
-    rgb(0 0 0 / 14%) 0px 4px 5px 0px, rgb(0 0 0 / 12%) 0px 1px 10px 0px;
-`;
