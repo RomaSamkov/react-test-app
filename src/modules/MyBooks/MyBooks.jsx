0e9235801fa22d89e3a5353f8ebc161d84a272ed
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-23 19:14:50 +0200

    add useAddBookMutation,useGetBooksQuery,useRemoveBookMutation

--- src/modules/MyBooks/MyBooks.jsx
+++ src/modules/MyBooks/MyBooks.jsx
@@ -1,51 +1,61 @@
-import {useEffect} from "react";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from 'react-redux';
 
-import Container from "./Container";
-import FormAddBook from "./FormAddBook";
-import BookList from "./BookList";
+import Container from './Container';
+import FormAddBook from './FormAddBook';
+import BookList from './BookList';
 
-import { fetchBooks, addBook, removeBook } from "../../redux/books/books-operations";
-import {setFilter} from "../../redux/filter/filter-actions";
-import { getFilteredBooks } from "../../redux/books/books-selectors";
-import { getFilter } from "../../redux/filter/filter-selectors";
+import { setFilter } from '../../redux/filter/filter-actions';
+import { getFilter } from '../../redux/filter/filter-selectors';
 
-import styles from "./my-books.module.css";
+import styles from './my-books.module.css';
+import {
+  useAddBookMutation,
+  useGetBooksQuery,
+  useRemoveBookMutation,
+} from 'redux/books/booksApi';
 
 const MyBooks = () => {
-    const books = useSelector(getFilteredBooks);
-    const filter = useSelector(getFilter);
+  const { data = [], isLoading, isError } = useGetBooksQuery();
+  // eslint-disable-next-line
+  const [addBook, addInfo] = useAddBookMutation();
+  // eslint-disable-next-line
+  const [removeBook, removeInfo] = useRemoveBookMutation();
 
-    const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
 
-    useEffect(()=> {
-        dispatch(fetchBooks())
-    }, [dispatch]);
+  const dispatch = useDispatch();
 
-    const onAddBook = (payload) => {
-        dispatch(addBook(payload))
-    }
+  const onAddBook = newBook => {
+    addBook(newBook).unwrap();
+  };
 
-    const onRemoveBook = (id) => {
-        dispatch(removeBook(id));
-    }
+  const onRemoveBook = id => {
+    removeBook(id).unwrap();
+  };
 
-    const onSetFilter = ({target}) => {
-        dispatch(setFilter(target.value))
-    }
+  const onSetFilter = ({ target }) => {
+    dispatch(setFilter(target.value));
+  };
 
-    return (
-        <div className={styles.books}>
-            <Container title="Добавить книгу">
-                <FormAddBook onSubmit={onAddBook} />
-            </Container>
-            <Container title="Список книг">
-                <input onChange={onSetFilter} value={filter} name="filter" type="text" placeholder="Введите название " />
-                <BookList books={books} removeBook={onRemoveBook}  />
-            </Container>
-        </div>
-    )
-
-}
+  return (
+    <div className={styles.books}>
+      {isLoading && <p>...Loading Books</p>}
+      {isError && <p>...Error load Books</p>}
+      <Container title="Добавить книгу">
+        <FormAddBook onSubmit={onAddBook} />
+      </Container>
+      <Container title="Список книг">
+        <input
+          onChange={onSetFilter}
+          value={filter}
+          name="filter"
+          type="text"
+          placeholder="Введите название "
+        />
+        <BookList books={data} removeBook={onRemoveBook} />
+      </Container>
+    </div>
+  );
+};
 
 export default MyBooks;
