fe1ca391844ecf2743b17b34258beacec2bf508c
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-23 19:15:14 +0200

    Update UserRoutes.jsx

--- src/UserRoutes.jsx
+++ src/UserRoutes.jsx
@@ -1,20 +1,18 @@
-import { lazy, Suspense } from "react";
-import {Routes, Route} from "react-router-dom";
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-const HomePage = lazy(()=> import('./pages/HomePage/HomePage'));
-const MyBooksPage = lazy(()=> import('./pages/MyBooksPage/MyBooksPage'));
-const MyFavoriteBooksPage = lazy(()=> import('./pages/MyFavoriteBooksPage/MyFavoriteBooksPage'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const MyBooksPage = lazy(() => import('./pages/MyBooksPage/MyBooksPage'));
 
 const UserRoutes = () => {
-    return (
-        <Suspense fallback={<p>....Load page</p>}>
-            <Routes> 
-                <Route path="/" element={<HomePage />} />
-                <Route path="/my-books" element={<MyBooksPage />} />
-                <Route path="/my-favorite-books" element={<MyFavoriteBooksPage />} />
-        </Routes>
-      </Suspense>
-    )
+  return (
+    <Suspense fallback={<p>....Load page</p>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/my-books" element={<MyBooksPage />} />
+      </Routes>
+    </Suspense>
+  );
 };
 
 export default UserRoutes;
