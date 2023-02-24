4a5d36a6d6b3f00d04fe86e5a58fa0463aaad31d
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-24 21:53:34 +0200

    Update BookList.jsx

--- src/modules/MyBooks/BookList/BookList.jsx
+++ src/modules/MyBooks/BookList/BookList.jsx
@@ -1,19 +1,28 @@
-import styles from "./book-list.module.css";
+import styles from './book-list.module.scss';
 
 const BookList = ({ books, removeBook }) => {
-    const elements = books.map(({ id, title, author }) => (
-        <li key={id}
-            className={styles.item}>{title}. Автор: {author}.
-            <button type="button" className={styles.btn}
-                onClick={() => removeBook(id)}>Удалить</button>
-        </li>
-    ));
+  const elements = books.map(({ id, title, author }) => (
+    <li key={id} className={styles.item}>
+      <div>
+        Title :<span className={styles.itemText}> "{title}".</span> Author:
+        <span className={styles.itemText}> {author}.</span>
+      </div>
 
-    return (
-        <ul>
-            {elements}
-        </ul>
-    )
-}
+      <button
+        type="button"
+        className={styles.btn}
+        onClick={() => removeBook(id)}
+      >
+        X
+      </button>
+    </li>
+  ));
+
+  return (
+    <ol type="1" className={styles.list}>
+      {elements}
+    </ol>
+  );
+};
 
 export default BookList;
