fae837fe3f7bb99f2a14108b2ec2ed867ef0db95
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-25 20:40:41 +0200

    Update Container.jsx

--- src/modules/MyBooks/Container/Container.jsx
+++ src/modules/MyBooks/Container/Container.jsx
@@ -1,19 +1,19 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-import styles from "./container.module.css";
+import styles from './container.module.scss';
 
-const Container = ({title, children}) => {
-    return (
-        <div className={styles.container}>
-            <h2 className={styles.title}>{title}</h2>
-            {children}
-        </div>
-    )
-}
+const Container = ({ title, children }) => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>{title}</h2>
+      {children}
+    </div>
+  );
+};
 
 export default Container;
 
 Container.propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.node,
-}
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
