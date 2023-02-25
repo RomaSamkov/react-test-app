4a9b28595a541f7ad41051caf48f2e22722635f3
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-25 20:41:01 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,13 @@
+import { NavLink } from 'react-router-dom';
+import styles from './HomePage.module.scss';
+
 const HomePage = () => {
-    return (
-        <div className="container">
-            <h2>HomePage page</h2>
-            
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Welcome Home Page !</h2>
+      <NavLink className={styles.link}>B o o k s</NavLink>
+    </div>
+  );
+};
 
 export default HomePage;
