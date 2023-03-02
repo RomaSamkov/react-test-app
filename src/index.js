a801cf0ba0c04d98aae4a4ff88b579c6e6828382
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-02 15:22:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import './fonts/BoyzRGrossShadowNF.ttf';
+import './fonts/ChanticleerRomanNF-Bold.ttf';
+import './fonts/ChanticleerRomanNF.ttf';
 
 import store from './redux/store';
 
